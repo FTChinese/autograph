e7e516ef7f9796de5a110de14b800999653c557c
weiguo.ni <neefrankie@gmail.com>
2017-01-24 15:13:06 +0800

    initial

---
+++ crawler/extract-links.js
@@ -0,0 +1,29 @@
+const cheerio = require('cheerio');
+const fs = require('mz/fs');
+
+function extractData(html) {
+  const $ = cheerio.load(html);
+  const rowEls = $('table.datasets').children('tr');
+  return rowEls.map((index, element) => {
+    const tdEls = $(element).children('td')
+    const csvName = tdEls.eq(0).children('a').text();
+    const lastModified = tdEls.eq(1).text();
+    return {
+      "name": csvName,
+      "lastModified": ""
+    }
+  }).get();
+}
+
+if (require.main === module) {
+  fs.readFile('autograph.html')
+    .then(content => {
+      const datasets = extractData(content)
+      return fs.writeFile('datasets.json', JSON.stringify(datasets, null, 4));
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
+
+module.exports = extractData;
---
+++ crawler/scrape.js
@@ -0,0 +1,33 @@
+const fs = require('mz/fs');
+const path = require('path');
+const got = require('got');
+
+const baseUrl = 'http://ig.ft.com/autograph';
+
+
+fs.readFile(path.resolve(__dirname, 'datasets.json'))
+  .then(data => {
+    return JSON.parse(data);
+  })
+  .then(datasets => {
+    return Promise.all(datasets.map(item => {
+      return got(`${baseUrl}/data/${item.name}`)
+        .then(res => {
+          return res.body
+        })
+        .then(body => {
+          return fs.writeFile(path.resolve(process.cwd(), `data/${item.name}`), body, 'utf8');
+        });
+    }));
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+// got(`${baseUrl}/data/cn-floor-space-construct.csv`)
+//   .then(res => {
+//     console.log(res.headers);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });

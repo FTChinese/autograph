184344b66c43a775ff1ff935a1a880123581c7b5
weiguo.ni <neefrankie@gmail.com>
2017-02-16 17:15:40 +0800

    test cron

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
 const co = require('co');
+const cron = require('node-cron');
 const endpoints = require('./endpoints');
 const chartsRender = require('./charts');
 
 function autoGraph() {
-    return co(function *() {
-// Fetch the HTML and extract data from it        
-        const {csvs, svgTimestamps} = yield endpoints.fetchHtml();
-// Parrallel scraping of all csv files and nightingale-config.json.        
-        const [, svgConfigs] = yield [
-            endpoints.fetchCsv(csvs),
-            endpoints.fetchJson()
-        ];
-// Use nightingale-config.json to render svg.
-// Use svgTimestamps scraped from HTML to build statistics of the svg to be used by server.        
-        yield chartsRender(svgConfigs, svgTimestamps);
-    })
-    .catch(err => {
-        console.log(err);
-    });
-}
-
-if (require.main == module) {
-    autoGraph()
+    return co(function*() {
+            // Fetch the HTML and extract data from it
+            const { csvs, svgTimestamps } = yield endpoints.fetchHtml();
+            // Parrallel scraping of all csv files and nightingale-config.json.        
+            const [, svgConfigs] = yield [
+                endpoints.fetchCsv(csvs),
+                endpoints.fetchJson()
+            ];
+            // Use nightingale-config.json to render svg.
+            // Use svgTimestamps scraped from HTML to build statistics of the svg to be used by server.        
+            yield chartsRender(svgConfigs, svgTimestamps);
+        })
         .catch(err => {
             console.log(err);
         });
 }
 
+// if (require.main == module) {
+//     autoGraph()
+//         .catch(err => {
+//             console.log(err);
+//         });
+// }
+
+cron.schedule('* * * * *', autoGraph);
+
 module.exports = autoGraph;
-
-

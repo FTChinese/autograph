765ca3397af67ed37ed89d81d6c7f835e0c6a684
weiguo.ni <neefrankie@gmail.com>
2017-02-20 18:11:31 +0800

    test remote

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,11 +10,14 @@ module.exports = {
       name      : "autograph-server",
       script    : "app.js",
       env: {
-        NODE_ENV: "development"
+        NODE_ENV: "development",
+        PORT: 3001
       },
       env_production : {
-        NODE_ENV: "production",
-        PORT: 4000
+        NODE_ENV: "production"
+      },
+      env_dev: {
+        NODE_ENV: "dev"
       }
     },
 
@@ -34,16 +37,16 @@ module.exports = {
       user : "node",
       host : "212.83.163.1",
       ref  : "origin/master",
-      repo : "git@github.com:repo.git",
+      repo : "git@github.com:FTChinese/autograph.git",
       path : "/var/www/production",
       "post-deploy" : "npm install && pm2 startOrRestart ecosystem.json --env production"
     },
     dev : {
       user : "node",
-      host : "212.83.163.1",
+      host : "node.corp.ftchinese.com",
       ref  : "origin/master",
-      repo : "git@github.com:repo.git",
-      path : "/var/www/development",
+      repo : "git@github.com:FTChinese/autograph.git",
+      path : "/data/git/ftacademy",
       "post-deploy" : "npm install && pm2 startOrRestart ecosystem.json --env dev",
       env  : {
         NODE_ENV: "dev"

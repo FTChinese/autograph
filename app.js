8bdec23b123163f52653a402ef467da010e719ed
weiguo.ni <neefrankie@gmail.com>
2017-02-28 10:00:03 +0800

    clear up unused pkg

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
-// const path = require('path');
 const serve = require('koa-static');
 const Koa = require('koa');
 const app = new Koa();
 const logger = require('koa-logger');
-const mount = require('koa-mount');
-const error = require('koa-error');
-const loadJsonFile = require('load-json-file');
-// const bodyParser = require('koa-bodyparser');
-// const payload = require('./server/payload.js');
 const render = require('./util/render.js');
 const buildArtifacts = require('./util/build-artifacts.js');
 const styles = require('./util/styles.js');
@@ -18,11 +12,6 @@ app.use(logger());
 app.use(serve('public', {
 	index: false
 }));
-
-
-// app.use(bodyParser());
-
-// app.use(mount('/payload', payload));
 
 app.use(function *() {
 	const today = new Date().toDateString();

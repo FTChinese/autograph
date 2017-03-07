7113c071473398d94991db548dbd0bfb9a7e526f
neefrankie <neefrankie@gmail.com>
2017-03-07 20:37:50 +0800

    six star pattern does not work

--- cron-job.js
+++ cron-job.js
@@ -4,7 +4,7 @@ const autoGraph = require('./index.js');
 
 const job = new CronJob({
 // start at 00:00 every day UTC+8       
-  cronTime: '00 00 6 * * *',
+  cronTime: '0 6 * * *',
   onTick: autoGraph,
   start: true,
 // See https://en.wikipedia.org/wiki/Time_in_China

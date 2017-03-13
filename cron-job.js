36e0c9fc4b246781278d4a1d71336219c9329a39
weiguo.ni <neefrankie@gmail.com>
2017-03-13 10:22:43 +0800

    set recur to every 2 hours between 0-8

--- cron-job.js
+++ cron-job.js
@@ -3,8 +3,8 @@ const autoGraph = require('./index.js');
 
 autoGraph();
 
-// Execute on 00:00 evry day UTC time.
+// Execute on evry day 2 hours between 0-8 UTC time.
 // const sched = later.parse.recur().on(0).hour();
-const sched = later.parse.recur().on(0).minute();
-console.log(later.schedule(sched).next(10));
+const sched = later.parse.recur().on(0,2,4,6,8).hour();
+// console.log(later.schedule(sched).next(10));
 later.setInterval(autoGraph, sched);

223d78bf6c6df3ec9efc54669ce39e6affc4bc2f
weiguo.ni <neefrankie@gmail.com>
2017-06-16 15:42:28 +0800

    migrated from sass to postcss

--- util/styles.js
+++ util/styles.js
@@ -1,22 +1,30 @@
-const pify = require('pify');
-const sass = pify(require('node-sass').render);
+const fs = require('fs-jetpack');
 const path = require('path');
-const uri = require('./uri');
+const postcss = require('postcss');
+const nested = require('postcss-nested');
+const cssvariables = require('postcss-css-variables');
+const cssnext = require('postcss-cssnext');
 
-async function styles(src=uri.chartScss) {
-  const result =  await sass({
-    file: src,
-    includePaths: ['bower_components'],
-    outputStyle: 'compressed',
-    precision: 10
-  });
-  return result.css.toString();
+async function styles(input, output) {
+  const src = path.resolve(process.cwd(), input);
+  const dest = path.resolve(process.cwd(), output);
+  const mycss = await fs.readAsync(src);
+  console.log(`Processing ${src}...`);
+
+  const result = postcss([
+      nested(),
+      cssvariables()
+    ])
+    .process(mycss)
+    .css;
+  console.log(`Geerated ${dest}`);
+  await fs.writeAsync(dest, result);
 }
 
-if (require.main == module) {
-  css()
+if (require.main === module) {
+  styles('client/chart.css', 'public/styles/chart.css')
     .catch(err => {
-        console.log(err);
+      console.log(err);
     });
 }
 
